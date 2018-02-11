eb47088bbd8f9e379b56b3617ddd8aa814af6ff4
朱梦迪 <montyzhu7@163.com>
2018-02-11 21:03:32 +0800

    settings

--- meituan/settings.py
+++ meituan/settings.py
@@ -121,3 +121,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 一般来说我们只要把静态文件放在 APP 中的 static 目录下，部署时用 python manage.py collectstatic 就可以把静态文件收集到（复制到） STATIC_ROOT 目录，
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+# 但是有时我们有一些共用的静态文件，这时候可以设置 STATICFILES_DIRS 另外弄一个文件夹，如下：
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "common_static"),
+    '/data/www/static/',
+)
+
+
+
+# media文件夹用来存放用户上传的文件，与权限有关，
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
